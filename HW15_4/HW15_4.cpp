9da3fea3e83025bf0d59b0d31deffbacfc8f5f3f
Alice Shu <alice@e26.ru>
2024-09-17 11:39:36 +0800

    Добавьте файлы проекта.

---
+++ HW15_4/HW15_4.cpp
@@ -0,0 +1,29 @@
+﻿// HW15_4.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int getValue()
+{
+    cout << "enter your number: ";
+    int a;
+    cin >> a;
+    return a;
+}
+
+void printOddNumbers(int a)
+{
+    cout << "Find Odd numbers: "<< "\n";
+    for (int count = 0; count < a+1; count +=2)
+    cout << count << "\n";
+}
+int main()
+{
+    int a = getValue();
+    printOddNumbers(a);
+
+    
+    
+}
